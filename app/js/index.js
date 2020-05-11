94937b8eeb2dabf1f190f4cf3117738e26295bbd
Vladislav Linnik <vlad.l@houstonapps.co>
2020-05-11 18:43:57 +0300

    changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,20 @@
-"use strict"
+// header menu
+$('.js-menuToggle').on('click', function() {
+    $('.js-menuList').toggleClass('menu__list--active');
+});
+$(window).resize(function() {
+    if (window.innerWidth <= 768) {
+        $('.js-menuList').removeClass('menu__list--active');
+    }
+});
 
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+        $('.header').addClass("scroll");
+    }
+    else{
+        $('.header').removeClass("scroll");
+    }
+});
